13164ca96c649d568f3c6884919df81deb30e770
TobeHyo <hyosang.kim@tobesoft.com>
2021-07-06 15:16:36 +0900

    set multi language

---
+++ languageFile/ko/ko.js
@@ -0,0 +1,14 @@
+if (!nexacro.translation) {
+    nexacro.translation = {};
+}
+nexacro.translation.ko = {
+    "인사": "안녕하세요 {{name}}",
+    "error": {
+        "invalid": "잘못 된 값"
+    },
+	"성명":"성명",
+	"찾기":"찾기",
+	"제목":"제목",
+	"다운로드":"다운로드",
+	"sample":"샘플"
+}
---
+++ languageFile/zh/zh-CN.js
@@ -0,0 +1,14 @@
+if (!nexacro.translation) {
+    nexacro.translation = {};
+}
+nexacro.translation.zh_CN = {
+    "인사": "你好 {{name}}",
+    "error": {
+        "invalid": "无效值"
+    },
+	"성명":"声明",
+	"찾기":"找到",
+	"제목":"书名",
+	"다운로드":"下载表格",
+	"sample":"样本"
+}
